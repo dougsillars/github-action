7cbf9fa77f514a8f4c691ff1cf99ec7f854195a1
Sébastien Charrier <scharrier@gmail.com>
2020-07-06 09:51:29 +0200

    Use doc option instead of deprecated id

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd /home
 gem install bump-cli
-$GEM_HOME/bin/bump $1 ${GITHUB_WORKSPACE}/$2 --id $3 --token $4 --validation $5
+$GEM_HOME/bin/bump $1 ${GITHUB_WORKSPACE}/$2 --doc $3 --token $4 --validation $5
